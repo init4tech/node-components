3f992a5a2fda43bd46e78cbec05a2b2204f8ffff
James Prestwich <james@prestwi.ch>
2025-08-01 16:02:26 -0400

    feat: node types readme (#3)
    * feat: node types readme
    
    * fix: lints in node-types
    
    * nit: remove codeowners funny

--- crates/node-types/src/lib.rs
+++ crates/node-types/src/lib.rs
@@ -1,4 +1,16 @@
-//! Types used by the signet node
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    clippy::missing_const_for_fn,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
 use reth::{
     primitives::EthPrimitives,
     providers::{
