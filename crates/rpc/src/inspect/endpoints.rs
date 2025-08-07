4269edf681f7fda65b89726e3574e787eca6da2f
James <james@prestwi.ch>
2025-08-07 11:08:36 -0400

    feat: hazmat router
    lint: clippy
    
    refactor: move PNT down

---
+++ crates/rpc/src/inspect/endpoints.rs
@@ -0,0 +1,37 @@
+use crate::{
+    RpcCtx,
+    inspect::db::{DbArgs, ListTableViewer},
+    utils::{await_jh_option_response, response_tri},
+};
+use ajj::{HandlerCtx, ResponsePayload};
+use reth_node_api::FullNodeComponents;
+use signet_node_types::Pnt;
+
+/// Handler for the `db` endpoint in the `inspect` module.
+pub(super) async fn db<Host, Signet>(
+    hctx: HandlerCtx,
+    args: DbArgs,
+    ctx: RpcCtx<Host, Signet>,
+) -> ResponsePayload<Box<serde_json::value::RawValue>, String>
+where
+    Host: FullNodeComponents,
+    Signet: Pnt,
+{
+    let task = async move {
+        let table: reth_db::Tables = response_tri!(args.table(), "invalid table name");
+
+        let viewer = ListTableViewer::new(ctx.signet().factory(), &args);
+
+        response_tri!(table.view(&viewer), "Failed to view table");
+
+        let Some(output) = viewer.take_output() else {
+            return ResponsePayload::internal_error_message(
+                "No output generated. The task may have panicked or been cancelled. This is a bug, please report it.".into(),
+            );
+        };
+
+        ResponsePayload::Success(output)
+    };
+
+    await_jh_option_response!(hctx.spawn_blocking(task))
+}
