7e83bc74f75ebccbf8efa78f43a609503c6af625
evalir <e@evalir.xyz>
2025-09-29 16:51:57 +0200

    rm unnecessary clone

--- crates/rpc/src/ctx/full.rs
+++ crates/rpc/src/ctx/full.rs
@@ -185,14 +185,12 @@ where
     where
         Tasks: TaskSpawner + Clone + 'static,
     {
-        SignetCtx::new(constants, provider, eth_config.clone(), tx_cache, spawner).map(|signet| {
-            Self {
-                host,
-                signet,
-                shared: SharedContext {
-                    tracing_semaphores: Semaphore::new(eth_config.max_tracing_requests).into(),
-                },
-            }
+        let tracing_semaphores = Semaphore::new(eth_config.max_tracing_requests).into();
+
+        SignetCtx::new(constants, provider, eth_config, tx_cache, spawner).map(|signet| Self {
+            host,
+            signet,
+            shared: SharedContext { tracing_semaphores },
         })
     }
 
